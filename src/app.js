2a0289f8e0dd0e2b796cbcf7d7cd7bb7449924bc
Maxim Samorukov <maxim.samorukov@gmail.com>
2021-02-11 22:18:41 +0300

    added files

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const jwt = require('jsonwebtoken');
+const app = express();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const port = process.env.PORT || 8080;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.status(200).send({ greeting: 'hello' });
+})
+
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`)
+})
