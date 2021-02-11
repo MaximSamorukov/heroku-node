86022f41468867a1e288425ab787c2273f37dbcf
Maxim Samorukov <maxim.samorukov@gmail.com>
2021-02-11 22:48:28 +0300

    added cors

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require("body-parser");
 const jwt = require('jsonwebtoken');
 const app = express();
+app.use(cors());
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const port = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.status(200).send({ greeting: 'hello' });
+  res.status(200).json({ greeting: 'hello' });
 })
 
 app.listen(port, () => {
